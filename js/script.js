18da7a6b8e6bbcfaa6cb9a7c04f7125893ef4498
Théau Dugueperoux <theaudugueperoux@gmail.com>
2021-12-13 09:01:57 +0100

    luck done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-let sommede = 0;
+let sommede = 1;
 let random = [];
+let lrandom = [];
+
+let pactol = parseInt(Math.random() * (1000000 - 1) + 1)
+document.getElementById('monney').innerHTML = pactol
 
 function loto(string, luck) {
+random = [];
     if (string.length !== 0) {
-        let array = string.split(' ');
-        if (array.length == 5) {
-            for (let i = 0; i <= 5; i++) {
-                random.push(Math.random() * (50 - 1) + 1).toString();
+        let array = string.split(' ')
+        if (array.length === 5) {
+            for (let i = 0; i < array.length; i++) {
+                random.push(parseInt(Math.random() * (49 - 1) + 1).toString());
             }
             for (let index = 0; index < array.length; index++) {
                 const element = array[index];
                 let j = random.indexOf(element);
                 if (j != -1)
                     sommede += 0.05;
+
                 if (j == index)
                     sommede += 0.1;
             }
@@ -21,9 +27,21 @@ function loto(string, luck) {
         }
     }
     if(luck.length !== 0){
-        let larray = string.split(' ');
-        if(larray.length == 5){
-            
+        let larray = string.split(' ')
+        if(larray.length === 2){
+           for (let i = 0; i <= 5; i++) {
+                lrandom.push(Math.random() * (10 - 1) + 1).toString();
+            }
+            for (let k = 0; k < larray.length; k++) {
+                const element = larray[k];
+                let bla = lrandom.indexOf(element);
+                if (bla != -1)
+                    sommede += 0.125;
+                if (bla == k)
+                    sommede += 0.25;
+            }
         }
     }
+    let prize = pactol * sommede
+    document.getElementById('prize').innerHTML = parseInt(prize)
 }
